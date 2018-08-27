f1bb224f9e3a3cf2692dc5fa0c699830ae5685b3
miriamfrank23 <miriamfrank23@gmail.com>
2018-08-27 17:48:59 -0400

    array practice

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,42 @@
+def sort_array_asc(array)
+  sorted = array.sort
+end
 
+def sort_array_desc(array)
+  sorted = array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  sorted = array.sort_by {|item| item.length}
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+reverse = array.reverse
+reverse
+end
+
+def kesha_maker(array)
+array.collect {|item| item.sub!(item[2], "$")}
+end
+
+def find_a(array)
+array.select {|item| item[0] == "a"}
+end
+
+def sum_array(array)
+array.sum
+end
+
+def add_s(array)
+array.each_with_index.collect{|element, index|
+  if index != 1
+    element.insert(-1,"s")
+  else element
+  end 
+  }
+end
